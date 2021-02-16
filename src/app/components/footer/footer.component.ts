3059421f4c9b4602e11ac4e60a8484d8424a7ca3
abdellatif ahammad <abdellatif.ahammad@gmail.com>
2021-02-17 00:23:49 +0100

    add the sidebar

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+public displayFooter:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  ngDoCheck():void{
+    console.log();
+    if(window.location.pathname!=="/login"&& window.location.pathname!=="/register"){
+      this.displayFooter = true;
+    }else{
+      this.displayFooter = false;
+    }
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import Vibrant from 'node-vibrant';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public palette:any;
+  public displayVideo:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+    // this.getVibrantColor('http://localhost:4200/assets/img/theme/movies.png')
   }
 
+  display(){
+    this.displayVideo=!this.displayVideo;
+  }
+
+  // getVibrantColor(url: string){
+  //   // Using builder
+  //   Vibrant.from(url).getPalette((err, palette) => {
+  //     console.log(palette)
+  //     this.palette = palette;
+  //   });
+  // }
+  
+  // styleContainer(): any {
+  //   console.log('palette', this.palette);
+  //   if (this.palette.LightVibrant) {
+  //     return { 'background-color': this.palette.LightVibrant.getHex(), 'border-color': 
+  //       this.palette.LightMuted.getHex(), 'color': '#000000' };
+  //   } else {
+  //     return { 'background-color': '#FFFFFF', 'border-color':        
+  //        this.palette.LightMuted.getHex(), 'color': '#000000' };
+  //   }
+  // }
+  
 }
